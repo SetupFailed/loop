49dce25ec21bffab41f85885e3bb12009d9cfd8d
SetupFailed <thomas.karlsson619@gmail.com>
2023-05-17 10:57:52 +0200

    JAG GJORDE EN LOOOOOOOOOP

---
+++ loop/Program.cs
@@ -0,0 +1,27 @@
+﻿bool running = true;
+int qCount = 0;
+
+
+Console.WriteLine("press Q... and just keep pressing");
+Console.ReadLine();
+
+while (running)
+{
+
+    if (Console.ReadKey(true).Key == ConsoleKey.Q)
+    {
+        qCount++;
+        Console.WriteLine($"{qCount}");
+    }
+
+    Console.Clear();
+
+    Console.WriteLine($"'Q' key count: {qCount}");
+    Console.WriteLine("Press 'ESC' to quit the game.");
+
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+    {
+        running = false;
+    }
+
+}
